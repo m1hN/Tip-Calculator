ffdaca8c9627aaba91bd433d79e056a5b50b4a32
m1hN <145584972+m1hN@users.noreply.github.com>
2024-01-11 10:19:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+// Tip Calculator
+
+// Saving elements as variables
+
+var percent1 = document.getElementById("div1");
+var percent2 = document.getElementById("div2");
+var percent3 = document.getElementById("div3");
+var percent4 = document.getElementById("div4");
+var skip = document.getElementById("div5");
+var custom = document.getElementById("div6");
+
+// Event Listeners
+
+percent1.addEventListener("click", applyPercent1);
+percent2.addEventListener("click", applyPercent2);
+percent3.addEventListener("click", applyPercent3);
+percent4.addEventListener("click", applyPercent4);
+skip.addEventListener("click", applyReset);
+custom.addEventListener("click", applyCustom);
+
+// Event Functions
+
+function applyPercent1() {
+  var amount = +document.getElementById("input").value;
+  total = amount * 0.18;
+  output = total + amount;
+  output = output.toFixed(2);
+
+  if (output != 0) {
+    document.getElementById("display1").innerHTML = `$${output}`;
+  } else {
+    document.getElementById("display1").innerHTML = "(add bill/amount)";
+  }
+  console.log(output);
+}
+
+function applyPercent2() {
+  var amount = +document.getElementById("input").value;
+  total = amount * 0.2;
+  output = total + amount;
+  output = output.toFixed(2);
+
+  if (output != 0) {
+    document.getElementById("display2").innerHTML = `$${output}`;
+  } else {
+    document.getElementById("display2").innerHTML = "(add bill/amount)";
+  }
+  console.log(output);
+}
+
+function applyPercent3() {
+  var amount = +document.getElementById("input").value;
+  total = amount * 0.25;
+  output = total + amount;
+  output = output.toFixed(2);
+
+  if (output != 0) {
+    document.getElementById("display3").innerHTML = `$${output}`;
+  } else {
+    document.getElementById("display3").innerHTML = "(add bill/amount)";
+  }
+  console.log(output);
+}
+
+function applyPercent4() {
+  var amount = +document.getElementById("input").value;
+  total = amount * 0.3;
+  output = total + amount;
+  output = output.toFixed(2);
+
+  if (output != 0) {
+    document.getElementById("display4").innerHTML = `$${output}`;
+  } else {
+    document.getElementById("display4").innerHTML = "(add bill/amount)";
+  }
+  console.log(output);
+}
+
+function applyReset() {
+  document.getElementById("input").value = "";
+  document.getElementById("display1").innerHTML = "(-------)";
+  document.getElementById("display2").innerHTML = "(-------)";
+  document.getElementById("display3").innerHTML = "(-------)";
+  document.getElementById("display4").innerHTML = "(-------)";
+  document.getElementById("custom").innerHTML = "(-------)";
+}
+
+function applyCustom() {
+  var amount = +document.getElementById("input").value;
+  let custom = +prompt("Enter your percentage you want to tip (only numbers):");
+  custom = custom / 100;
+
+  let total = amount * custom;
+  output = total + amount;
+  output = output.toFixed(2);
+
+  if (output == 0) {
+    document.getElementById("custom").innerHTML = "(add bill/amount)";
+  } else {
+    document.getElementById("custom").innerHTML = `$${output}`;
+  }
+  console.log(output);
+}
